8410889b7d97319238e8e8eea0b18848645401f7
CWONG038 <89695216+CWONG038@users.noreply.github.com>
2021-09-22 07:46:30 +0800

    Add files via upload

---
+++ format_amount.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 12:22:25 2021
+
+@author: chris
+"""
+
+
+
+def format_amount(amount):
+    result = 0
+    while True:
+        try:
+            print("Please enter a valid number.")
+            result = "${:,.2f}".format(float(input(amount)))
+            
+        except ValueError:
+            if result != float():
+                print("Please enter a valid number.")
+                result = "${:,.2f}".format(float(input(amount)))
+      
+        else:
+             return result
+
+
+            
+amount_spent = format_amount("Enter spending amount")
+print(amount_spent)
+
+
+
